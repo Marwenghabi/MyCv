8687d38611bc772f42b16394b83ba3fcf8ab8f19
Marwen Ghabi <marwen.ghabi@beta-systemes.com>
2022-11-24 09:11:14 +0100

    ajouter des traductions sur le contenu de cv

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const FALLBACK_LOCAL = "fr";
     const translations = {
         // English translations
         "en": {
-           'experience':'Experience'
+           'experience':'Work Experience',
+           'descriptioninfo' :"Je suis très ambitieux, je veux suivre différents types de nouvelles technologies, que ce soit dans la programmation ou dans d'autres domaines. Je suis précis, direct et patient. Je souhaite soutenir les autres et travailler en groupes. Je souhaite mettre à jour mes connaissances et apprendre d'autres personnes qui ont de l'expérience."
         },
         // francais translations
         "fr": {
-            'experience':'Experiences'
+            'experience':'Experience',
+            'descriptioninfo' :"I am very ambitious, I want to follow different kinds of new technologies, whether in programming or in other fields. I am precise, direct and patient. I want to support others and work in groups. I want to update my knowledge and learn from other people who have experience."
         },
     };
         document
